cad4c200990e3baa5d8d20a417d13b65bdd53c23
sdabdullah <ab.faishal@gmail.com>
2025-11-11 21:10:11 +0600

    AuthProvider function added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { use } from 'react';
 import { FiLogOut } from 'react-icons/fi';
 import { Link, NavLink } from 'react-router';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+
+    const { user } = use(AuthContext);
+
     const navLinks = <>
         <NavLink to='/' className='font-semibold p-3 text-[17px]' ><li> Home </li></NavLink>
         <NavLink to='/add-habit' className='p-3 text-[17px] font-semibold'><li>Add Habit</li></NavLink>
@@ -40,43 +44,45 @@ const Navbar = () => {
                     </ul>
                 </div>
 
-                <div className='flex items-center gap-3'>
-                    <div className="dropdown z-10 dropdown-end space-x-2">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Tailwind CSS Navbar component"
-                                    src='https://scontent.fdac177-1.fna.fbcdn.net/v/t39.30808-1/431791220_2140807236256918_636841911573142717_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=106&ccb=1-7&_nc_sid=1d2534&_nc_ohc=NG_0Y8bVxVsQ7kNvwGn96ET&_nc_oc=AdnxQlz_cW5a8nIvQtUxpXIEEGx6Yk4eIe-ioORYujW-8IAtKmiKSqHFcQkGq-pqTZ0&_nc_zt=24&_nc_ht=scontent.fdac177-1.fna&_nc_gid=mDaDwwzobZYUPuvpJvK7Uw&oh=00_AfjCGL53ZBc9luNhIk6T03sYjpaQPnPyivQoMZk9XSiHHg&oe=6917EAF7' />
+                {
+                    user ?
+                        <div className='flex items-center gap-3'>
+                            <div className="dropdown z-10 dropdown-end space-x-2">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img
+                                            alt="Tailwind CSS Navbar component"
+                                            src='https://scontent.fdac177-1.fna.fbcdn.net/v/t39.30808-1/431791220_2140807236256918_636841911573142717_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=106&ccb=1-7&_nc_sid=1d2534&_nc_ohc=NG_0Y8bVxVsQ7kNvwGn96ET&_nc_oc=AdnxQlz_cW5a8nIvQtUxpXIEEGx6Yk4eIe-ioORYujW-8IAtKmiKSqHFcQkGq-pqTZ0&_nc_zt=24&_nc_ht=scontent.fdac177-1.fna&_nc_gid=mDaDwwzobZYUPuvpJvK7Uw&oh=00_AfjCGL53ZBc9luNhIk6T03sYjpaQPnPyivQoMZk9XSiHHg&oe=6917EAF7' />
+                                    </div>
+                                </div>
+
+                                <ul tabIndex="-1" className="border menu menu-sm dropdown-content bg-base-100 shadow-sm rounded-box mt-3 w-52 p-2">
+
+                                    <div className='space-y-2 text-[16px] px-3'>
+
+                                        <p>Name</p>
+                                        <p>email</p>
+
+                                        <p className='cursor-pointer flex items-center gap-2'><FiLogOut />Logout</p>
+                                    </div>
+
+                                    {/* <button onClick={logOutUser} className="rounded-2xl font-semibold border-0 text-md">Logout</button> */}
+                                </ul>
                             </div>
+                        </div> :
+
+
+
+                        <div className='flex items-center space-x-2'>
+                            <NavLink to='/login' className='btn border-2 bg-white rounded-md font-semibold text-[16px]'>Login</NavLink>
+
+                            <NavLink to='/register' className="bg-[#7b3aec] btn border-gray-300 text-white rounded-md  ">Signup</NavLink>
                         </div>
-
-                        <ul tabIndex="-1" className="border menu menu-sm dropdown-content bg-base-100 shadow-sm rounded-box mt-3 w-52 p-2">
-
-                            <div className='space-y-2 text-[16px] px-3'>
-
-                                <p>Name</p>
-                                <p>email</p>
-
-                                <p className='cursor-pointer flex items-center gap-2'><FiLogOut />Logout</p>
-                            </div>
-
-                            {/* <button onClick={logOutUser} className="rounded-2xl font-semibold border-0 text-md">Logout</button> */}
-                        </ul>
-                    </div>
-
-                    {/* <Link to='/my-profile'>
-                        <img className='ring-primary ring-offset-base-100 w-10 lg:w-14 rounded-full border-2 border-[#22d3ee]' src={user?.photoURL} alt="" />
-                    </Link> */}
-
-                </div>
+                }
 
 
 
-                <div className='flex items-center space-x-2'>
-                    <NavLink to='/login' className='btn border-2 bg-white rounded-md font-semibold text-[16px]'>Login</NavLink>
 
-                    <NavLink to='/register' className="bg-[#7b3aec] btn border-gray-300 text-white rounded-md  ">Signup</NavLink>
-                </div>
 
             </div>
         </div>
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    useEffect(() =>{
+        const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unsubcribe()
+        }
+    }, [])
+
+    const authInfo = {
+        createUser,
+        signInUser,
+        signInWithGoogle,
+        user,
+        loading,
+    }
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
